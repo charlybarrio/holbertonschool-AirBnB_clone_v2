584affefd20c79b7aac1f0d4e1ababdfe7c0d543
charlybarrio <5757@holbertonschool.com>
2023-03-26 12:46:31 -0300

    Fix setuptest

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,11 @@
 -- Creates database, user and configures it
-CREATE DATABASE IF NOT EXISTS hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
-CREATE USER IF NOT EXISTS 'hbnb_test' @ 'localhost'
+CREATE USER IF NOT EXISTS 'hbnb_test' @ 'localhost';
 
-SET PASSWORD FOR 'hbnb_test' @ 'localhost' = 'hbnb_test_pwd'
+SET PASSWORD FOR 'hbnb_test' @ 'localhost' = 'hbnb_test_pwd';
 
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO ' hbnb_test ' @ ' localhost '
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO ' hbnb_test ' @ ' localhost ';
 
 GRANT
-SELECT ON performance_schema.* TO ' hbnb_test ' @ ' localhost '
+SELECT ON performance_schema.* TO ' hbnb_test ' @ ' localhost ';
