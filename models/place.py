0fe92749402a9823a5d6eae3ac2d70ae3f6bae17
charlybarrio <5757@holbertonschool.com>
2023-03-26 13:30:37 -0300

    places

--- models/place.py
+++ models/place.py
@@ -13,7 +13,7 @@ place_amenity = Table('place_amenity', base.metadata,
                              primary_key=True, nullable=False),
                       Column(
                         'amenity_id', String(60), ForeignKey("amenities.id"),
-                            primary_key=True, nullable=False)
+                        primary_key=True, nullable=False)
                       )
 
 
