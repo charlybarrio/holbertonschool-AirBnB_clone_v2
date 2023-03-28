f4e31e4630cb81bddc2e621921c185578967d0a2
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-28 05:49:28 -0300

    pyfix style

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,13 +14,14 @@ def state_city_print():
     from models import storage
     from models.state import State
 
-    states_dict = storage.all(State)
+    states_dict = storage.all(State).values()
     states_city_dict = {}
 
-    for state in states_dict.values():
+    for state in states_dict:
         states_city_dict[state] = state.cities
 
-    return render_template('8-cities_by_states.html', states_city_dict=states_city_dict)
+    return render_template('8-cities_by_states.html',
+                           states_city_dict=states_city_dict)
 
 
 @app.teardown_appcontext
