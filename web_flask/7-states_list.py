e9ca7d416622e381ffcb04ccc47f0691a095a20a
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-28 05:00:37 -0300

    checker test1

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 """ Task 7 module """
 from flask import Flask, render_template
-from models import storage
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Returns a sorted list of states"""
-    states = storage.all(states).values()
-    states_sorted = sorted(states, key=lambda state: state.name)
+@app.route('/states_list', strict_slashes=False)
+def state_print():
+    """prints states"""
 
-    return render_template('7-states_list.html', states_list=states_sorted)
+    from models import storage
+    from models.state import State
+    import operator
+
+    states = storage.all(State)
+
+    new_list = []
+    for value in states.values():
+        new_list.append(value)
+
+    sorted_list = sorted(new_list, key=operator.attrgetter('name'))
+
+    return render_template('7-states_list.html', states_list=sorted_list)
 
 
 @ app.teardown_appcontext
