be5c998741a7d5410498ca2a57dc95364de7a081
charlybarrio <5757@holbertonstudents.com>
2023-03-29 18:43:15 -0700

    task7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
-""" Task 7 module """
+"""task 8"""i
+
 from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def state_print():
-    """prints states"""
+def state_list():
+    """print state and cities"""
+    states_dict = storage.all(State).values()
+    states_city_dict = {}
 
-    from models import storage
-    from models.state import State
-    import operator
+    for state in states_dict:
+        states_city_dict[state] = state.cities
 
-    states = storage.all(State)
-
-    new_list = []
-    for value in states.values():
-        new_list.append(value)
-
-    sorted_list = sorted(new_list, key=operator.attrgetter('name'))
-
-    return render_template('7-states_list.html', states_list=sorted_list)
-
-
-@ app.teardown_appcontext
-def closing(dummy):
-    """closes alchemy session"""
-
-    from models import storage
-
-    storage.close()
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    return render_template('7-states_list.html',
+                           states_city_dict=states_city_dict)
