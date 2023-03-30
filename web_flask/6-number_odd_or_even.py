5392f9b278d4c2f85cffbc29b7f7c2402049c537
charlybarrio <5757@holbertonstudents.com>
2023-03-29 18:22:25 -0700

    task6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Task 6 module """
+""" Task 5 module """
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -26,27 +26,26 @@ def string(text):
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def show(text='is cool'):
-    """returns default text or give text"""
+    """returns default text or given text"""
     return f'Python {text}'
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """returns input if number"""
+    """returns given input if number"""
     return f'{n} is a number'
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def template_number(n):
-    """returns number in html template"""
-    return render_template('5-number.html')
+    """returns html page if n is an int"""
+    return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def odd_even(n):
-    """returns html template with odd or even text"""
-    return render_template('6-number_odd_or_even.html', number=n)
-
+def odd_number(n):
+    """return HTML page if n is an int"""
+    return render_template('6-number_odd_or_even.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
